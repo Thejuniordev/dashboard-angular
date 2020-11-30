6771879eb80a005423fa1f08e6a58f2f76766fe5
Darcy Junior <darcy.santos@compasso.com.br>
2020-11-30 10:02:28 -0300

    Resolve bug in import jwt decode

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import * as jwt_decode from 'jwt-decode';
+import jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
